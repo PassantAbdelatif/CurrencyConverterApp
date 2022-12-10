7d2b22081a2df51da0e0a37ede533115202d4425
Passant Abdelatif <passantabdelatif@gmail.com>
2022-12-10 12:36:58 +0200

    init commit

---
+++ CurrencyConverterApp/CurrencyConverterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverterApp
+//
+//  Created by Passant Abdelatif on 10/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
